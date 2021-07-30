ffed108ebcb1d6faee50627db744ffb78d135c76
Manuel Mondin <manuel.mondin@studenti.unitn.it>
2021-07-30 18:11:38 +0200

    Aggiunte liste in homepage sottoforma di exposed dropdown menu

--- Pommidori/app/src/main/java/com/unitn/lpsmt/group13/pommidori/Homepage.java
+++ Pommidori/app/src/main/java/com/unitn/lpsmt/group13/pommidori/Homepage.java
@@ -11,6 +11,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationView;
@@ -23,6 +25,11 @@ public class Homepage extends AppCompatActivity implements NavigationView.OnNavi
     Toolbar toolbar;
 
     Button calendario;
+    Button btnScadenze;
+    Button btnSessioni;
+
+    AutoCompleteTextView listaScadenze;
+    AutoCompleteTextView listaSessioni;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +41,24 @@ public class Homepage extends AppCompatActivity implements NavigationView.OnNavi
         navigationView = findViewById(R.id.nav_view);
         toolbar = findViewById(R.id.homeToolbar);
 
+        btnScadenze = (Button) findViewById( R.id.btn_dropdown_scadenze);
+        btnSessioni = (Button) findViewById( R.id.btn_dropdown_sessioni);
+
+        listaScadenze = (AutoCompleteTextView) findViewById( R.id.dropdown_scadenze);
+        listaSessioni = (AutoCompleteTextView) findViewById( R.id.dropdown_sessioni);
+
         calendario = findViewById(R.id.hp_calendario);
 
         //Metodi
         setNavigationDrawerMenu();
         setButtonListeners();
+        setDropDownLists();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setDropDownLists();
     }
 
     //Chiudere il navigation drawer con il pulsante indietro
@@ -90,5 +110,42 @@ public class Homepage extends AppCompatActivity implements NavigationView.OnNavi
                 startActivity(new Intent(Homepage.this,Calendario.class));
             }
         });
+
+        btnScadenze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if( listaScadenze.isPopupShowing()){
+                    listaScadenze.dismissDropDown();
+                }else{
+                    listaScadenze.showDropDown();
+                }
+            }
+        });
+
+        btnSessioni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if( listaSessioni.isPopupShowing()){
+                    listaSessioni.dismissDropDown();
+                }else{
+                    listaSessioni.showDropDown();
+                }
+            }
+        });
+
+    }
+
+    private void setDropDownLists(){
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.planets_array,
+                R.layout.dropdown_item
+        );
+
+        listaScadenze.setText( adapter.getItem(0));
+        listaSessioni.setText( adapter.getItem(0));
+
+        listaScadenze.setAdapter( adapter);
+        listaSessioni.setAdapter( adapter);
     }
 }
